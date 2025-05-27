a0fb5d6f278c3fb4453a1bd254ca70ccdf8b47a3
Vrinda Vinod <vrindavinod@digitalocean.com>
2025-05-27 14:37:18 +0530

    Changes

--- leaderboard_backend/leaderboard_backend/settings.py
+++ leaderboard_backend/leaderboard_backend/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'leaderboard_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']  
+STATIC_ROOT = BASE_DIR / 'staticfiles' 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
